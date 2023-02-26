088507a718169a7c03ca07235b9c1791259d5c18
Error-402 <guilherme.lopes.freitas@gmail.com>
2023-02-26 17:37:26 -0300

    wtf fix

--- bash.sh
+++ bash.sh
@@ -9,18 +9,16 @@ fi
 
 #get info trough .env
 wait_bd_connection(){
-    while ! wget host.docker.internal:3309 -v; do 
+    while ! wget host.docker.internal:3309 --quiet; do 
         echo "waiting db conn..."
         sleep 1 
     done
 }
 
-wait_bd_connection
+#wait_bd_connection
 
 echo "db conn established"
 
 chmod -R 777 .
 
-cd app/
-
-uvicorn app:app --host 0.0.0.0 --port ${APP_PORT}
+uvicorn app.app:app --host 0.0.0.0 --port 8080
